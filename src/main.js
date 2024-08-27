e0501fe5fa99d4b09eecf65a94f501a021f91529
Gabriel Henrique <work.gabriel12@gmail.com>
2024-08-27 12:32:55 -0300

    html & css added

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import dayjs from "dayjs.js";
+
+// JS
+import "./modules/schedule"
---
+++ src/modules/clientForm.js
@@ -0,0 +1,6 @@
+const newSchedule = document.getElementById("newSchedule")
+const modal = document.getElementById("modal")
+
+newSchedule.addEventListener("click", () => {
+  modal.showModal()
+})
